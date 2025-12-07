140ca66b2dc99add2c51e4e16b41f2ff07abd00f
Hoàng Mạnh Tuấn <tino2deptrai@gmail.com>
2025-12-07 20:56:43 +0700

    ye

--- Assets/_Game/Scripts/Player/StateMachine/PlayerDeathState.cs
+++ Assets/_Game/Scripts/Player/StateMachine/PlayerDeathState.cs
@@ -14,11 +14,11 @@ public class PlayerDeathState : PlayerState
         player.SetVelocityX(0);
         player.SetVelocityY(0);
 
-        // Tắt vật lý để không bị rơi hoặc bị đẩy nữa (tùy game design)
-        player.Rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+        //// Tắt vật lý để không bị rơi hoặc bị đẩy nữa (tùy game design)
+        //player.Rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
 
-        // Vô hiệu hóa Collider để không bị quái đánh thêm
-        player.GetComponent<Collider2D>().enabled = false;
+        //// Vô hiệu hóa Collider để không bị quái đánh thêm
+        //player.GetComponent<Collider2D>().enabled = false;
 
         Debug.Log("GAME OVER");
     }
