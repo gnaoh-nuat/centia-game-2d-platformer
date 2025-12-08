d8d00611665acb6a19146ba3d711c7634524b28a
Hoàng Mạnh Tuấn <tino2deptrai@gmail.com>
2025-12-08 21:22:16 +0700

    Audio Manager

---
+++ Assets/_Game/Scripts/Core/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Audio Settings")]
+    [SerializeField] private AudioClip _levelMusic; 
+
+    private void Start()
+    {
+        if (_levelMusic != null)
+        {
+            AudioManager.Instance.PlayMusic(_levelMusic);
+        }
+    }
+}
